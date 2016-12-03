6187d65bfdb8da9cc7c131817e56df0c3f79576f
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-12-03 17:48:46 +0900

    Add HTTP request on get token

--- app/src/main/java/com/nyampass/simpleled/MyFirebaseIdService.kt
+++ app/src/main/java/com/nyampass/simpleled/MyFirebaseIdService.kt
@@ -6,11 +6,18 @@ import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.iid.FirebaseInstanceIdService
 
 import android.content.ContentValues.TAG
+import android.provider.Settings
+import com.github.kittinunf.fuel.httpPost
 
 class MyFirebaseIdService : FirebaseInstanceIdService() {
     override fun onTokenRefresh() {
         val refreshedToken = FirebaseInstanceId.getInstance().token
         Log.d(TAG, "Refreshed token: " + refreshedToken!!)
+
+        val androidId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID)
+
+        "http://192.168.11.68:3000/api/tokens/android/$androidId".httpPost(listOf("token" to refreshedToken))
+                .response { request, response, result ->
+                }
     }
 }
-
